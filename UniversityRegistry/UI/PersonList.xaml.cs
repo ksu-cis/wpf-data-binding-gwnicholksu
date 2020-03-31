c0bd2ce39dc8be8d7bf3bf1ad2c3d5d9553d7988
Grant Nichol <gwnichol@ksu.edu>
2020-03-31 13:32:44 -0500

    Finish in class work

--- UniversityRegistry/UI/PersonList.xaml.cs
+++ UniversityRegistry/UI/PersonList.xaml.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -18,9 +16,25 @@ namespace UniversityRegistry.UI
     /// </summary>
     public partial class PersonList : UserControl
     {
+        /// <summary>
+        /// A proxy event handler
+        /// </summary>
+        public event SelectionChangedEventHandler SelectionChanged;
+
+
         public PersonList()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// A proxy event listener that passes on SelectionChanged events
+        /// </summary>
+        /// <param name="sender">The ListView that had its selection changed</param>
+        /// <param name="e">The event arguments</param>
+        private void OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            SelectionChanged?.Invoke(this, e);
         }
     }
 }
